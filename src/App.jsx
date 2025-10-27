9feb95da826ec6f440d55174651bd986bbbf6763
Ariful Islam <arifulq234@gmail.com>
2025-10-28 03:46:06 +0600

    user add functionality added both bacend and frontend also

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,34 @@
 
+import { use, useState } from 'react'
 import './App.css'
 import User from './Components/User'
 const userPromise=fetch('http://localhost:5000/users').then(res=>res.json())
 
+
 function App() {
-const handleSubmit=(e)=>{
-  e.preventDefault()
-  const name=e.target.name.value;
-  const email=e.target.email.value;
-  console.log(name,email)
-  const nesuser={name,email}
-  fetch('http://localhost:5000/users',{
-    method:'POST',
-    headers:{
-      'Content-Type':'application/json'
-    },
-    body:JSON.stringify(nesuser)
-  }).then(res=>res.json()).then(
-    data=>{
-      console.log(data)
-    }
-  )
-}
+
+// const handleSubmit=(e)=>{
+//   e.preventDefault()
+//   const name=e.target.name.value;
+//   const email=e.target.email.value;
+//   console.log(name,email)
+//   const nesuser={name,email}
+//   fetch('http://localhost:5000/users',{
+//     method:'POST',
+//     headers:{
+//       'Content-Type':'application/json'
+//     },
+//     body:JSON.stringify(nesuser)
+//   }).then(res=>res.json()).then(
+//     data=>{
+//      setUserPromise(prev=>[...prev,data])
+//     }
+//   )
+// }
 
   return (
     <>
-    <div>
+    {/* <div>
       <form onSubmit={handleSubmit}>
         <label>name</label><br />
         <input type="text" name="name" id="" /><br />
@@ -33,7 +36,7 @@ const handleSubmit=(e)=>{
         <input type="email" name="email" id="" /><br />
         <input type="submit" value="Add User"  /><br />
       </form>
-    </div>
+    </div> */}
     <h1>Users</h1>
         <User userPromise={userPromise}></User>
     </>
--- src/Components/User.jsx
+++ src/Components/User.jsx
@@ -1,9 +1,38 @@
-import React, { use } from 'react';
+import React, { use, useState } from 'react';
 
 const User = ({userPromise}) => {
-    const users=use(userPromise)
+    const usersFromBacend=use(userPromise)
+    const [users,setusers]=useState(usersFromBacend)
+    const handleSubmit=(e)=>{
+  e.preventDefault()
+  const name=e.target.name.value;
+  const email=e.target.email.value;
+  console.log(name,email)
+  const nesuser={name,email}
+  fetch('http://localhost:5000/users',{
+    method:'POST',
+    headers:{
+      'Content-Type':'application/json'
+    },
+    body:JSON.stringify(nesuser)
+  }).then(res=>res.json()).then(
+    data=>{
+    setusers(prev=>[...prev,data])
+    }
+  )
+}
+ 
     return (
         <div>
+            <div>
+      <form onSubmit={handleSubmit}>
+        <label>name</label><br />
+        <input type="text" name="name" id="" /><br />
+        <label>Email</label><br />
+        <input type="email" name="email" id="" /><br />
+        <input type="submit" value="Add User"  /><br />
+      </form>
+    </div>
             {
                 users.map(user=>{
                     return<div key={user.id}>
